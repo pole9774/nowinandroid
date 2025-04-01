5d95a1a013eb1b37c7646af75e844ce1d88029a4
Alessandro Poletti <pole97@live.it>
2025-04-01 17:56:43 +0200

    some tests (2)

--- app/src/androidTest/java/com/google/samples/apps/nowinandroid/ui/AppiumTest.java
+++ app/src/androidTest/java/com/google/samples/apps/nowinandroid/ui/AppiumTest.java
@@ -2,6 +2,8 @@ package com.google.samples.apps.nowinandroid.ui;
 
 import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.PointerInput;
+import org.openqa.selenium.interactions.Sequence;
 import org.testng.Assert;
 import org.testng.annotations.*;
 import org.testng.annotations.BeforeClass;
@@ -11,6 +13,7 @@ import java.net.MalformedURLException;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.time.Duration;
+import java.util.Collections;
 import java.util.List;
 
 import io.appium.java_client.AppiumBy;
@@ -20,6 +23,44 @@ import io.appium.java_client.android.options.UiAutomator2Options;
 public class AppiumTest {
 
     private AndroidDriver driver;
+
+    // Utility function to swipe vertically (top to bottom)
+    public void swipeUp(String containerXPath) {
+        WebElement container = driver.findElement(AppiumBy.xpath(containerXPath));
+
+        int startX = container.getLocation().getX() + (container.getSize().getWidth() / 2);
+        int startY = container.getLocation().getY() + (int) (container.getSize().getHeight() * 0.8);
+        int endY = container.getLocation().getY() + (int) (container.getSize().getHeight() * 0.2);
+
+        PointerInput finger = new PointerInput(PointerInput.Kind.TOUCH, "finger1");
+        Sequence swipe = new Sequence(finger, 1);
+
+        swipe.addAction(finger.createPointerMove(Duration.ZERO, PointerInput.Origin.viewport(), startX, startY)); // Start
+        swipe.addAction(finger.createPointerDown(PointerInput.MouseButton.LEFT.asArg()));
+        swipe.addAction(finger.createPointerMove(Duration.ofMillis(1000), PointerInput.Origin.viewport(), startX, endY)); // Move
+        swipe.addAction(finger.createPointerUp(PointerInput.MouseButton.LEFT.asArg()));
+
+        driver.perform(Collections.singletonList(swipe));
+    }
+
+    // Utility function to swipe vertically (bottom to top)
+    public void swipeDown(String containerXPath) {
+        WebElement container = driver.findElement(AppiumBy.xpath(containerXPath));
+
+        int startX = container.getLocation().getX() + (container.getSize().getWidth() / 2);
+        int startY = container.getLocation().getY() + (int) (container.getSize().getHeight() * 0.2);
+        int endY = container.getLocation().getY() + (int) (container.getSize().getHeight() * 0.8);
+
+        PointerInput finger = new PointerInput(PointerInput.Kind.TOUCH, "finger1");
+        Sequence swipe = new Sequence(finger, 1);
+
+        swipe.addAction(finger.createPointerMove(Duration.ZERO, PointerInput.Origin.viewport(), startX, startY)); // Start
+        swipe.addAction(finger.createPointerDown(PointerInput.MouseButton.LEFT.asArg()));
+        swipe.addAction(finger.createPointerMove(Duration.ofMillis(1000), PointerInput.Origin.viewport(), startX, endY)); // Move
+        swipe.addAction(finger.createPointerUp(PointerInput.MouseButton.LEFT.asArg()));
+
+        driver.perform(Collections.singletonList(swipe));
+    }
 
     @BeforeClass
     public void setup() throws URISyntaxException, MalformedURLException {
@@ -361,6 +402,108 @@ public class AppiumTest {
             // Verify we are in 'For you'
             WebElement app_name = driver.findElement(AppiumBy.xpath("//android.widget.TextView[@text=\"Now in Android\"]"));
             Assert.assertTrue(app_name.isDisplayed(), "App Name is not displayed!");
+        } catch (NoSuchElementException e) {
+            Assert.fail("Element not found: " + e.getMessage());
+        } catch (Exception e) {
+            Assert.fail("Unexpected error: " + e.getMessage());
+        }
+    }
+
+    @Test
+    public void navigationBar_multipleBackStackInterests() {
+        int maxSwipes = 10;
+        int swipes = 0;
+
+        try {
+            // Search and select the 'Interests' tab
+            WebElement interests = driver.findElement(AppiumBy.xpath("//O0.r0/android.view.View/android.view.View/android.view.View/android.view.View/android.view.View[2]/android.view.View/android.view.View[3]"));
+            Assert.assertTrue(interests.isDisplayed(), "'Interests' is not visible!");
+            interests.click();
+
+            // Xpath of the view that contains the topics
+            String containerXPath = "//android.view.View[@resource-id='interests:topics']";
+
+            // Swipe vertically until the last topic is on screen
+            while (driver.findElements(AppiumBy.xpath("//android.widget.TextView[@text=\"Wear OS\"]")).isEmpty() && swipes < maxSwipes) {
+                swipeUp(containerXPath);
+                swipes++;
+            }
+
+            // Verify that last topic is displayed and click it
+            WebElement last_topic = driver.findElement(AppiumBy.xpath("//android.widget.TextView[@text=\"Wear OS\"]"));
+            Assert.assertTrue(last_topic.isDisplayed(), "Last topic is not displayed!");
+            last_topic.click();
+
+            // Search and select the 'For you' tab
+            WebElement for_you = driver.findElement(AppiumBy.xpath("//O0.r0/android.view.View/android.view.View/android.view.View/android.view.View/android.view.View[2]/android.view.View/android.view.View[1]"));
+            Assert.assertTrue(for_you.isDisplayed(), "'For you' is not visible!");
+            for_you.click();
+
+            // Search and select the 'Interests' tab
+            WebElement interests2 = driver.findElement(AppiumBy.xpath("//O0.r0/android.view.View/android.view.View/android.view.View/android.view.View/android.view.View[2]/android.view.View/android.view.View[3]"));
+            Assert.assertTrue(interests2.isDisplayed(), "'Interests' is not visible!");
+            interests.click();
+
+            // Verify that last topic is still opened, then search for the back arrow and click it
+            WebElement last_topic_title = driver.findElement(AppiumBy.xpath("//android.widget.TextView[@text=\"Wear OS\"]"));
+            Assert.assertTrue(last_topic_title.isDisplayed(), "Last topic title is not displayed!");
+            WebElement last_topic_description = driver.findElement(AppiumBy.xpath("//android.widget.TextView[@text=\"The latest news on app development for Wear OS.\"]"));
+            Assert.assertTrue(last_topic_description.isDisplayed(), "Last topic description is not displayed!");
+
+            WebElement back_arrow = driver.findElement(AppiumBy.accessibilityId("Back"));
+            Assert.assertTrue(back_arrow.isDisplayed(), "Back arrow is not displayed!");
+            back_arrow.click();
+
+            // Search and select the 'For you' tab
+            WebElement for_you2 = driver.findElement(AppiumBy.xpath("//O0.r0/android.view.View/android.view.View/android.view.View/android.view.View/android.view.View[2]/android.view.View/android.view.View[1]"));
+            Assert.assertTrue(for_you2.isDisplayed(), "'For you' is not visible!");
+            for_you.click();
+        } catch (NoSuchElementException e) {
+            Assert.fail("Element not found: " + e.getMessage());
+        } catch (Exception e) {
+            Assert.fail("Unexpected error: " + e.getMessage());
+        }
+    }
+
+    @Test
+    public void navigatingToTopicFromForYou_showsTopicDetails() {
+        int maxSwipes = 20;
+        int swipes = 0;
+
+        try {
+            // Search and check the 'Headlines' topic
+            WebElement sample_topic = driver.findElement(AppiumBy.xpath("//android.widget.TextView[@text=\"Headlines\"]"));
+            Assert.assertTrue(sample_topic.isDisplayed(), "Sample topic 'Headlines' is not visible!");
+            sample_topic.click();
+
+            // Swipe until topic tag is found
+            String news_view_xpath = "//android.view.View[@resource-id=\"forYou:feed\"]";
+            while (driver.findElements(AppiumBy.xpath("//android.widget.TextView[@content-desc=\"Headlines is followed\"]")).isEmpty() && swipes < maxSwipes) {
+                swipeUp(news_view_xpath);
+                swipes++;
+            }
+
+            // Click on topic tag
+            WebElement topic_tag = driver.findElement(AppiumBy.xpath("//android.widget.TextView[@content-desc=\"Headlines is followed\"]"));
+            Assert.assertTrue(topic_tag.isDisplayed(), "Topic tag is not displayed!");
+            topic_tag.click();
+
+            // Verify we are on topic page, then go back
+            WebElement topic_title = driver.findElement(AppiumBy.xpath("//android.widget.TextView[@text=\"Headlines\"]"));
+            Assert.assertTrue(topic_title.isDisplayed(), "Topic title is not displayed!");
+            WebElement back_arrow = driver.findElement(AppiumBy.accessibilityId("Back"));
+            Assert.assertTrue(back_arrow.isDisplayed(), "Back arrow is not displayed!");
+            back_arrow.click();
+
+            // Swipe up and uncheck topic
+            swipes = 0;
+            while (driver.findElements(AppiumBy.xpath("//android.view.View[@content-desc=\"Headlines\"]")).isEmpty() && swipes < maxSwipes) {
+                swipeDown(news_view_xpath);
+                swipes++;
+            }
+            WebElement sample_topic2 = driver.findElement(AppiumBy.xpath("//android.view.View[@content-desc=\"Headlines\"]"));
+            Assert.assertTrue(sample_topic2.isDisplayed(), "Sample topic 'Headlines' is not visible!");
+            sample_topic2.click();
         } catch (NoSuchElementException e) {
             Assert.fail("Element not found: " + e.getMessage());
         } catch (Exception e) {
